8f32de315921356707f2293ec73cdec68974e96b
Florian Bernard <florian.bernard@openairlines.com>
2023-04-03 19:52:13 +0200

    Add pages for python app

--- python-app/app.py
+++ python-app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello (secured) Python !"
+    return render_template('hello.html')
+
+@app.route('/headers')
+def headers():
+    return render_template('headers.html',headers=request.headers.items())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
