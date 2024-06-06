0b570f25ac270b1c34ea089fb49235877716b834
Florian Bernard <florian@flob.fr>
2024-06-06 21:58:05 +0200

    update java, keycloak

--- python-app/app.py
+++ python-app/app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return render_template('hello.html')
 
+
 @app.route('/headers')
 def headers():
-    return render_template('headers.html',headers=request.headers.items())
+    return render_template('headers.html', headers=request.headers.items())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
