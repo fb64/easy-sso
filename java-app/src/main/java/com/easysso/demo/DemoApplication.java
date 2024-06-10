ac03b297272446cc12cca747be5b79585ca1af15
Florian Bernard <florian@flob.fr>
2024-06-10 21:57:21 +0200

    reformat java code

--- java-app/src/main/java/com/easysso/demo/DemoApplication.java
+++ java-app/src/main/java/com/easysso/demo/DemoApplication.java
@@ -14,19 +14,22 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 
 @SpringBootApplication
 public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public RouterFunction<ServerResponse> route(){
-		return RouterFunctions
-				.route(GET("/"),
-						request -> ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("hello.html"))
-				.andRoute(GET("/headers"), req ->
-						ServerResponse.ok().contentType(MediaType.TEXT_HTML)
-								.render("headers.html", Collections.singletonMap("headers",req.headers().asHttpHeaders())));
-	}
+    @Bean
+    public RouterFunction<ServerResponse> route() {
+        return RouterFunctions
+                .route(GET("/"), req ->
+                        ServerResponse.ok()
+                                .contentType(MediaType.TEXT_HTML)
+                                .render("hello.html"))
+                .andRoute(GET("/headers"), req ->
+                        ServerResponse.ok()
+                                .contentType(MediaType.TEXT_HTML)
+                                .render("headers.html", Collections.singletonMap("headers", req.headers().asHttpHeaders())));
+    }
 }
 
 
